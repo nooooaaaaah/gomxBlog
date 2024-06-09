a90bff78746412033b6529972b76231de719ec84
Noah Spielman <nspielman@live.com>
2024-06-08 20:52:48 -0600

    line numbers

--- ui/static/js/site.js
+++ ui/static/js/site.js
@@ -34,3 +34,55 @@ document.querySelectorAll("#menu-modal a").forEach(function (link) {
     menuModal.classList.remove("opacity-100");
   });
 });
+
+// Function to add copy button to each code block
+function addCopyButton() {
+  var codeBlocks = document.querySelectorAll(".mx-auto.break-words pre");
+  codeBlocks.forEach(function (block) {
+    // Wrap the code block in a container
+    var codeContainer = document.createElement("div");
+    codeContainer.className = "relative border border-midnight-700";
+    block.parentNode.insertBefore(codeContainer, block);
+    codeContainer.appendChild(block);
+
+    // Style the pre block for better visuals
+    block.className += " bg-opacity-95 rounded-lg";
+
+    // Create the copy button
+    var copyButton = document.createElement("button");
+    copyButton.textContent = "Copy";
+    copyButton.className =
+      "absolute top-0 right-0 m-2 bg-transparent text-peach-400 border border-peach-400 rounded-lg py-1 px-3 transition-colors duration-200 hover:bg-peach-400 hover:text-midnight-900 focus:outline-none";
+    copyButton.onclick = function () {
+      copyContent(block);
+    };
+    codeContainer.appendChild(copyButton);
+  });
+}
+
+// Function to copy content of the code block
+function copyContent(block) {
+  var content = block.innerText;
+  var copyTextarea = document.createElement("textarea");
+  copyTextarea.value = content;
+  document.body.appendChild(copyTextarea);
+  copyTextarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(copyTextarea);
+  alert("Code copied to clipboard!");
+}
+
+if (window.location.pathname.includes("/blog")) {
+  document.addEventListener("DOMContentLoaded", (event) => {
+    // Highlight code
+    document.querySelectorAll("pre code").forEach((block) => {
+      hljs.highlightElement(block);
+    });
+
+    // Add line numbers
+    document.querySelectorAll("pre code").forEach((block) => {
+      hljs.lineNumbersBlock(block);
+    });
+  });
+  addCopyButton();
+}
